07425966657bed9e81da083c3f4801612c6db093
shivamsk00 <shivamsk00>
2025-11-06 23:28:58 +0530

    remove pedding

--- src/renderer/src/pages/dashboard/Dashboard.jsx
+++ src/renderer/src/pages/dashboard/Dashboard.jsx
@@ -62,7 +62,7 @@ const Dashboard = () => {
 
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4 sm:p-6 lg:p-8">
+    <div className="min-h-screen ">
       <div className="max-w-8xl mx-auto space-y-6">
         
         {/* Top KPI Cards */}
